e0f969b15352e12e6e1611fd5857a892da30aee6
burturt <31748545+burturt@users.noreply.github.com>
2021-01-26 14:25:22 -0800

    Asdf

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ sudo apt install -y aptitude sl
 sudo chmod 700 /usr/games/sl
 sudo mv /usr/games/sl /usr/local/bin/
 echo 'root:smuhsd123' | sudo chpasswd
+sudo chmod 700 /usr/local/bin/sl
 clear
 rm ~/files/start.sh
